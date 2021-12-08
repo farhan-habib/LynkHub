580c8ac484728063c28fa947c5e4bb117221cf7d
Farhan Habib <f.habibca@gmail.com>
2021-12-08 09:24:04 -0800

    Adding new user method to DbUtil

--- server/util/dbUtil.js
+++ server/util/dbUtil.js
@@ -1,12 +1,15 @@
 //TODO: CHECK IF SINGlETON OBJECT IMPLEMENTED CORRECTLY
 const path = require("path");
 const sqlite3 = require('sqlite3').verbose();
+const passwordUtil = require("./passwordUtils.js");
+
 
 let dbPath = path.join(".", "data", "userData.sqlite");
+
+
 class dbUtil {
 
 	constructor() {
-		this.x = [];
 		if (!dbUtil.instance) {
 			this.db = new sqlite3.Database(dbPath, (err) => {
 				if (err) { console.error(err.message); }
@@ -16,15 +19,29 @@ class dbUtil {
 		}
 		return dbUtil.instance;
 	}
-	password() {
-		this.x.push("");
-		return this.x;
-	}
 
+	//internal functions:
+
+
+
+
+
+	createNewUser(username, password) {
+		const salt = passwordUtil.generateSalt();
+		const hashed_password = passwordUtil.hashPassword(password, salt);
+		this.db.serialize(function () {
+			var stmt = this.db.run("INSERT INTO users (username, hashed_password, salt) VALUES ($username, $hashed_password, $salt)", {
+				$username: username,
+				$hashed_password: hashed_password,
+				$salt: salt,
+			});
+		})
+		return stmt.lastID;
+	}
 }
 
 
-const instance = new dbUtil();
+const instance = new dbUtil()
 Object.freeze(instance);
 
 module.exports = instance;
