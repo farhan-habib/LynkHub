8177f167cf660dfcd298bbdd0a2dfbdb6c4e9362
Farhan Habib <f.habibca@gmail.com>
2021-12-16 13:45:22 -0800

    Adding BST

---
+++ data_structures/binarySearchTree.js
@@ -0,0 +1,53 @@
+
+
+
+class BinarySeachTree {
+	static Node  = class {
+		constructor(value) {
+			this.data = value
+			this.left = null
+			this.right = null
+		}
+	}
+	constructor() {
+		this.root = null
+	}
+
+	insert(data) {
+
+		
+		let node = new BinarySeachTree.Node(data);
+		if (this.root === null){ this.root = node; return;}  //if there is no root, set new node to root 
+		let current = this.root;
+
+		while (current) {
+			if (data.localeCompare(current.data)) {
+				if (current.left === null) {current.left = node; return;} //if the left child of current is null set the new node to it
+				current = current.left;
+			} else {
+				if (current.right === null) {current.right = node; return; } //if the right child of current is null set the new node to it
+				current = current.right;
+			}
+		}
+	}
+
+	inOrderTraversal() {
+		console.log(this.root);
+		const inOrderTraversalHelper = function(node){
+			
+			if(node.left) inOrderTraversalHelper(node.left);
+			console.log(node.data);
+			if(node.right) inOrderTraversalHelper(node.right);
+		}
+		inOrderTraversalHelper(this.root);
+	}
+}
+
+
+
+const bst = new BinarySeachTree();
+
+bst.insert("bob")
+bst.insert("joe")
+bst.insert("a2343424")
+bst.inOrderTraversal();
